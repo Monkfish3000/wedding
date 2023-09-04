cb16ebc866945a7cbe7d5e23eb6974954b994e76
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-09-04 17:01:04 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import viteLogo from '/vite.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Logo" />
+        </a>
+      </div>
+      <h1>Michael & Jade</h1>
+    </>
+  );
+}
+
+export default App;
