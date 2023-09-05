a3656867206a4609a56a99b059f60aa797d72e7f
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2023-09-05 18:09:00 +0200

    colours themed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import { useState } from 'react';
+
 import logo from '/jandm.png';
+
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <>
-      <main>
-        <img src={logo} className="logo" alt="Logo" />
-        <h1>Michael & Jade</h1>
+      <main
+        className={`h-screen w-screen ${
+          darkMode ? 'bg-pink' : 'bg-white opacity-75'
+        } flex flex-col items-center justify-center`}
+      >
+        <img
+          src={logo}
+          className="logo"
+          alt="Logo"
+          onClick={() => setDarkMode(!darkMode)}
+        />
+        <h1
+          className={`${
+            darkMode
+              ? 'text-white text-4xl opacity-80 border-b border-white'
+              : 'text-light dark:text-dark text-4xl border-b border-pink'
+          }`}
+        >
+          Michael & Jade
+        </h1>
       </main>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        light: 'white', // Background color for light mode
+        dark: 'rose-500', // Background color for dark mode
+      },
+      textColor: {
+        light: '#ebbad2', // Text color for light mode
+        dark: 'white', // Text color for dark mode
+      },
+    },
+    colors: {
+      pink: '#ebbad2', // custom pink
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['dark'],
+      textColor: ['dark'],
+    },
   },
   plugins: [],
 };
